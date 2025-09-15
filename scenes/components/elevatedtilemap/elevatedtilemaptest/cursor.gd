972e4aa2ab09740701d073bf1b4764be80910b27
Chris George <chrismathew98@gmail.com>
2025-09-15 17:34:19 -0400

    refactor: change references of 'terrain' to 'surface'

--- scenes/components/elevatedtilemap/elevatedtilemaptest/cursor.gd
+++ scenes/components/elevatedtilemap/elevatedtilemaptest/cursor.gd
@@ -8,7 +8,7 @@ var cursor_position_int: Vector2i:
 		return Vector2i(floor(cursor_position.x), floor(cursor_position.y));
 
 func _ready() -> void:
-	var res = terrain_tile_map.TerrainToGlobal(cursor_position_int);
+	var res = terrain_tile_map.SurfaceToGlobal(cursor_position_int);
 	if res != null:
 		global_position = res;
 
@@ -17,6 +17,6 @@ func _process(delta: float) -> void:
 	cursor_position += input_vector*delta*10;
 	cursor_position.x = min(max(0, cursor_position.x), 24);
 	cursor_position.y = min(max(0, cursor_position.y), 24);
-	var res = terrain_tile_map.TerrainToGlobal(cursor_position_int);
+	var res = terrain_tile_map.SurfaceToGlobal(cursor_position_int);
 	if res != null:
 		global_position = res;
