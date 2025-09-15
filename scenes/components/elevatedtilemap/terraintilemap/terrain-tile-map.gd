43d1bcc159bbb2890d8a0a8db4b43ca7f3769876
Chris George <chrismathew98@gmail.com>
2025-09-15 17:34:19 -0400

    refactor: change references of 'terrain' to 'surface'

--- scenes/components/elevatedtilemap/terraintilemap/terrain-tile-map.gd
+++ scenes/components/elevatedtilemap/terraintilemap/terrain-tile-map.gd
@@ -6,12 +6,12 @@ extends ElevatedTileMap
 const IS_TERRAIN = "is_terrain";
 const HEIGHT_MULTIPLIER = "height_multiplier";
 
-var terrain_map: Dictionary[Vector2i, int] = {};
+var surface_map: Dictionary[Vector2i, int] = {};
 
 func _ready() -> void:
-	GenerateTerrain();
+	GenerateSurface();
 
-func GenerateTerrain():
+func GenerateSurface():
 	for tile_map_layer: CustomTileMapLayer in get_children():
 		for cell_coords: Vector2i in tile_map_layer.get_used_cells():
 			var tile_data: TileData = tile_map_layer.get_cell_tile_data(cell_coords);
@@ -19,16 +19,16 @@ func GenerateTerrain():
 			if tile_data.has_custom_data(IS_TERRAIN):
 				is_terrain = tile_data.get_custom_data(IS_TERRAIN);
 			if is_terrain:
-				if terrain_map.has(cell_coords):
-					terrain_map[cell_coords] = max(terrain_map[cell_coords], tile_map_layer.layer);
+				if surface_map.has(cell_coords):
+					surface_map[cell_coords] = max(surface_map[cell_coords], tile_map_layer.layer);
 				else:
-					terrain_map[cell_coords] = tile_map_layer.layer;
+					surface_map[cell_coords] = tile_map_layer.layer;
 
-func TerrainToLocal(terrain_position: Vector2i):
-	if !terrain_map.has(terrain_position):
+func SurfaceToLocal(surface_position: Vector2i):
+	if !surface_map.has(surface_position):
 		return null;
-	var z: int = terrain_map[terrain_position];
-	var coords: Vector3i = Vector3i(terrain_position.x, terrain_position.y, z);
+	var z: int = surface_map[surface_position];
+	var coords: Vector3i = Vector3i(surface_position.x, surface_position.y, z);
 	var center_location: Vector2 = MapToLocal(coords);
 	var tile_data: TileData = GetCellTileData(coords);
 	var height_multiplier: float = 1;
@@ -37,8 +37,8 @@ func TerrainToLocal(terrain_position: Vector2i):
 	var inverse_height_multiplier: float = 1-height_multiplier;
 	return center_location + Vector2(0, tile_z*inverse_height_multiplier);
 
-func TerrainToGlobal(terrain_position: Vector2i):
-	var terrain_to_local_res = TerrainToLocal(terrain_position);
+func SurfaceToGlobal(surface_position: Vector2i):
+	var terrain_to_local_res = SurfaceToLocal(surface_position);
 	if terrain_to_local_res == null:
 		return null;
 	return to_global(terrain_to_local_res);
