4d2a50fb0563e6826e4eade4dd5e14bfcd8c093a
Chris George <chrismathew98@gmail.com>
2025-09-17 22:33:27 -0400

    feat: use `surface_type` instead of `is_terrain` to determine if a (x, y) is a surface

--- scenes/components/elevatedtilemap/terraintilemap/terrain-tile-map.gd
+++ scenes/components/elevatedtilemap/terraintilemap/terrain-tile-map.gd
@@ -3,25 +3,47 @@ class_name TerrainTileMap
 extends ElevatedTileMap
 
 # Custom Data Layer Names
-const IS_TERRAIN = "is_terrain";
 const HEIGHT_MULTIPLIER = "height_multiplier";
+const SURFACE_TYPE = "surface_type"
+
+static var default_validator := func(_a): return true;
+
+static func generate_custom_data_helper(layer_name: String, default_value: Variant, validator: Callable = default_validator) -> Callable:
+	return func(tile_data: TileData):
+		var res = default_value;
+		if tile_data.has_custom_data(layer_name):
+			var unvalidated = tile_data.get_custom_data(layer_name);
+			if validator.call(unvalidated) == true:
+				res = tile_data.get_custom_data(layer_name);
+		return res;
+
+static var get_height_multiplier := generate_custom_data_helper(HEIGHT_MULTIPLIER, 1.0);
+
+static var surface_type_validator := func(a): return a >= 0 and a < Enums.SurfaceType.MAX;
+static var get_surface_type := generate_custom_data_helper(SURFACE_TYPE, Enums.SurfaceType.SURFACE, surface_type_validator);
 
 var surface_map: Dictionary[Vector2i, int] = {};
 
 func _ready() -> void:
 	generate_surface();
 
+static func sort_custom_tile_layers(a: CustomTileMapLayer, b: CustomTileMapLayer) -> bool:
+	return a.layer < b.layer;
+
 func generate_surface():
-	for tile_map_layer: CustomTileMapLayer in get_children():
+	var tile_map_layers := get_children();
+	tile_map_layers.sort_custom(sort_custom_tile_layers);
+	for tile_map_layer: CustomTileMapLayer in tile_map_layers:
 		for cell_coords: Vector2i in tile_map_layer.get_used_cells():
 			var tile_data := tile_map_layer.get_cell_tile_data(cell_coords);
-			var is_terrain := false;
-			if tile_data.has_custom_data(IS_TERRAIN):
-				is_terrain = tile_data.get_custom_data(IS_TERRAIN);
-			if is_terrain:
-				if surface_map.has(cell_coords):
-					surface_map[cell_coords] = max(surface_map[cell_coords], tile_map_layer.layer);
-				else:
+			var surface_type: Enums.SurfaceType = get_surface_type.call(tile_data);
+			match surface_type:
+				Enums.SurfaceType.BLOCKING:
+					if surface_map.has(cell_coords):
+						surface_map.erase(cell_coords);
+				Enums.SurfaceType.STATIC_ENTITY:
+					pass ;
+				Enums.SurfaceType.SURFACE:
 					surface_map[cell_coords] = tile_map_layer.layer;
 
 func surface_to_local(surface_position: Vector2i):
@@ -31,9 +53,7 @@ func surface_to_local(surface_position: Vector2i):
 	var coords := Vector3i(surface_position.x, surface_position.y, z);
 	var center_location := map_to_local(coords);
 	var tile_data := get_cell_tile_data(coords);
-	var height_multiplier := 1.0;
-	if tile_data.has_custom_data(HEIGHT_MULTIPLIER):
-		height_multiplier = tile_data.get_custom_data(HEIGHT_MULTIPLIER);
+	var height_multiplier: float = get_height_multiplier.call(tile_data);
 	var height_adjustment := 1 - height_multiplier;
 	return center_location + Vector2(0, tile_z * height_adjustment);
 
