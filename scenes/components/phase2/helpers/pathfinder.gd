b159e8e3b0700d5884fd1eb71afdf7a2018e6d80
EclipsedStar <83985361+EclipsedStar0@users.noreply.github.com>
2025-09-21 18:45:35 -0400

    feat: rebuild astar3d connections locally
    - In Pathfinder.gd:
    - - Changed the surrounding_vectors function to now only return vectors that are within surface_map (as in their x-y combo is in surface_map to ensure the vectors aren't out of bounds)
    - - Added the _get_point_id function to convert a Vector2i  to point_id for Pathfinder
    - - Added the _rebuild_point_with_id function which 'rebuilds' the AStar3D point connections for a specific point
    - - Added the _localised_rebuild_point function which 'rebuilds' the AStar3D point connections in a radius around a specific point
    - - Removed the 'if vector in identifier_map:' saftey check in the _rebuild_connections function due to the certainty that any vectors provided will be in identifier_map due to being in surface_map.

--- scenes/components/phase2/helpers/pathfinder.gd
+++ scenes/components/phase2/helpers/pathfinder.gd
@@ -18,8 +18,78 @@ func surrounding_vectors(provided_coordinate:Vector3, height_l=-1, height_u=1) -
 		for row in range(provided_coordinate.y-1, provided_coordinate.y+2): # +2 because the upper bound of range is exclusive
 			for column in range(provided_coordinate.x-1, provided_coordinate.x+2):
 				if row != provided_coordinate.y and column != provided_coordinate.x and height != provided_coordinate.z:
-					return_arr.append(Vector3(column, row, height))
+					if Vector2i(provided_coordinate.x, provided_coordinate.y) in surface_map:
+						return_arr.append(Vector3(column, row, height))
 	return return_arr
+	
+func _get_point_id(unit_pos:Vector2i)->int:
+	if unit_pos in surface_map:
+		var conv_coord:Vector3 = Vector3(unit_pos.x, unit_pos.y, surface_map.get(unit_pos))
+		if conv_coord in identifier_map:
+			return identifier_map.get(conv_coord)
+		else:
+			print("[ WARN]: When fetching point using unit posistion [" + str(unit_pos.x) + "][" + str(unit_pos.y) + "], point was not in [identifier_map]. Resorting to closest_point.")
+			var tmp:int =  get_closest_point(conv_coord)
+			identifier_map[conv_coord] = tmp
+			return tmp 
+	else:
+		print("[ERROR]: Unit provided posistion [" + str(unit_pos.x) + "][" + str(unit_pos.y) + "] is not in surface_map")
+		return -1
+
+func _rebuild_point_with_id(point_id:int):
+	var old_coord = inverted_identifier_map.get(point_id)
+	if old_coord == null:
+		if has_point(point_id):
+			print("[ WARN]: When rebuilding point [" + str(point_id) + "], point exists but is not stored in [inverted_identifier_map]. Fixing.")
+			inverted_identifier_map[point_id] = get_point_position(point_id)
+			old_coord = inverted_identifier_map.get(point_id)
+		else:
+			print("[ERROR]: When rebuilding point [" + str(point_id) + "], point does NOT exist")
+			return
+	remove_point(point_id)
+	surface_map = tile_map_ref._provide_surface_map()
+	var conv_coord = Vector3(old_coord.x, old_coord.y, surface_map.get(old_coord.x, old_coord.y))
+	identifier_map.erase(old_coord)
+	add_point(point_id, conv_coord, 1.0)
+	identifier_map[conv_coord] = point_id
+	inverted_identifier_map[point_id] = conv_coord
+	var coord_id = identifier_map.get(conv_coord)
+	var vector_grab = surrounding_vectors(conv_coord)
+	for vector in vector_grab:
+		if vector in identifier_map:
+			connect_points(coord_id, identifier_map.get(vector))
+	var lower_vectors = surrounding_vectors(conv_coord, -2, -2)
+	for vector in lower_vectors:
+		if vector in identifier_map:
+			connect_points(coord_id, identifier_map.get(vector), false)
+
+func _localised_rebuild_point(center_point_id:int, radius=1):
+	surface_map = tile_map_ref._provide_surface_map()
+	var old_coord_arr = surrounding_vectors(inverted_identifier_map.get(center_point_id), -radius, radius)
+	old_coord_arr.append(center_point_id)
+	var tmp_coord_arr = []
+	for coord in old_coord_arr:
+		if coord in identifier_map:
+			tmp_coord_arr.append([coord, identifier_map.get(coord)])
+			remove_point(coord)
+	var new_coord_arr = []
+	for entry in tmp_coord_arr: 
+		var new_coord = Vector3(entry[0].x, entry[0].y, surface_map.get(entry[0].x, entry[0].y))
+		new_coord_arr.append(new_coord)
+		identifier_map.erase(entry[0])
+		identifier_map[new_coord] = entry[1]
+		inverted_identifier_map[entry[1]] = new_coord
+		add_point(entry[1], new_coord, 1.0)
+	for coordinate in new_coord_arr:
+		var coord_id = identifier_map.get(coordinate)
+		var vector_grab = surrounding_vectors(coordinate)
+		for vector in vector_grab:
+			if vector in identifier_map:
+				connect_points(coord_id, identifier_map.get(vector))
+		var lower_vectors = surrounding_vectors(coordinate, -2, -2)
+		for vector in lower_vectors:
+			if vector in identifier_map:
+				connect_points(coord_id, identifier_map.get(vector), false)
 	
 func _rebuild_connections():
 	clear()
@@ -37,12 +107,12 @@ func _rebuild_connections():
 		var coord_id = identifier_map.get(coordinate)
 		var vector_grab = surrounding_vectors(coordinate)
 		for vector in vector_grab:
-			if vector in identifier_map:
-				connect_points(coord_id, identifier_map.get(vector))
+			# Don't need a saftey check for if it's in identifier_map cause identifier_map = surface_map and surrounding_vectors only return ones that are 'within' bounds of surface_map
+			connect_points(coord_id, identifier_map.get(vector))
 		var lower_vectors = surrounding_vectors(coordinate, -2, -2)
 		for vector in lower_vectors:
-			if vector in identifier_map:
-				connect_points(coord_id, identifier_map.get(vector), false)
+			# Don't need a saftey check for if it's in identifier_map cause identifier_map = surface_map and surrounding_vectors only return ones that are 'within' bounds of surface_map
+			connect_points(coord_id, identifier_map.get(vector), false)
 	
 func _return_path(provided_coordinate:Vector3i, provided_target:Vector3i):
 	var vec_3 = Vector3(provided_coordinate.x, provided_coordinate.y, provided_coordinate.z)
