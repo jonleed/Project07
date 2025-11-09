jonleed/Project07

commit 14e33bbeadbd87ed88c232056150bc0916b77901

feat: state_machine accordant to discussion
- In enemy.gd:
- - Implements the state machine accordant to discussions.
- - - State Machine presently *appears* to work when tested; Only tested for attack/moving but support should work as it's nearly a mirror of attacking but with some variables swapped out
- - - renames _execute_turn() to be execute_turn in accordance to turn_manager expectations
- - - removes previous state_machine files from commit 5fd69675f6479ca6281405c0314931e6114a90c8
- - State machine begins via calling the idle_state()
- - Renamed enum who_to_heal to who_to_support
- - Renamed type_of_unit.ATTACK to type_of_unit.ATTACKER
- - renamed unit_acting_behaviour to unit_type_behaviour
- In pathfinder,gd
- - Implement additional safeguards for out-of-map points (do not try to find the closest point to the invalid location, return -INF and mark them as invalid)

scenes/components/phase2/unit/hostile_unit.gd
@@ -19,14 +19,13 @@ enum who_to_attack {
 	CLOSEST
 }
 
-enum who_to_heal {
-	LAST_TO_TAKE_DAMAGE,
+enum who_to_support {
 	LOWEST_HEALTH,
 	CLOSEST
 }
 
 enum type_of_unit {
-	ATTACK,
+	ATTACKER,
 	SUPPORTER
 }
 
@@ -34,8 +33,8 @@ enum type_of_unit {
 var retreat_location_behaviour:int = where_to_retreat_to.TO_CLOSEST_FRIEND
 var retreat_behaviour:int = when_to_retreat.LOW_HEALTH
 var attack_behaviour:int = who_to_attack.CLOSEST
-var unit_acting_behaviour:int = type_of_unit.ATTACK
-var cached_movement_path:PackedVector2Array = []
+var support_behaviour:int = who_to_support.CLOSEST
+var unit_type_behaviour:int = type_of_unit.ATTACKER
 var last_unit_to_damage_me:Entity = null
 
 func _ready() -> void:
@@ -54,28 +53,321 @@ func set_attack_behaviour(provided_behaviour:int) -> void:
 		attack_behaviour = provided_behaviour
 		
 
-func _execute_turn() -> void:
-	var created_plan:Plan = $State_Machine.make_plan()
-	var plan_data:Dictionary[int, Dictionary] = created_plan.get_plan()
+func execute_turn() -> void:
+	cached_parent = get_parent()
+	action_failed = false
+	movement_failed = false
+	cached_attack_action = null
+	cached_support_action = null
+	enemy_that_we_care_about = null
+	friend_that_we_care_about = null
+	move_count = move_max
+	action_count = action_max
+	idle_state()
 	
-	var last_action_succeeded:bool = true
-	for index in range(0, created_plan.get_num_planned_actions()):
-		var plan_increment_data:Dictionary[String, Variant] = plan_data[index]
-		var planned_action:Action = plan_increment_data.get("Action")
-		if planned_action is Moveaction:
-			var planned_movement_path:PackedVector2Array = plan_increment_data.get("Path")
-			cached_parent.move_unit_via_path(self, planned_movement_path, true)
-			if cur_pos != Vector2i(planned_movement_path[-1]):
-				last_action_succeeded = false
-				break
-		elif planned_action is Attackaction or planned_action is Healaction:
-			var affected_unit:Entity = plan_increment_data.get("Targetted Unit")
-			if affected_unit.cur_pos not in planned_action.range_pattern.calculate_affected_tiles_from_center(cur_pos):
-				last_action_succeeded = false
-				break
-			else:
-				use_action(planned_action, affected_unit)
 	
-	# Set all to 0 to ensure the unit is flagged as having completed their turn
-	move_count = 0
+var action_failed:bool = false
+var movement_failed:bool = false
+var enemy_that_we_care_about:Entity = null
+var friend_that_we_care_about:Entity = null
+
+func determine_enemy_we_care_about() -> void:
+	if attack_behaviour == Hostile_Unit.who_to_attack.LAST_TO_DAMAGE:
+		if last_unit_to_damage_me != null:
+			enemy_that_we_care_about = last_unit_to_damage_me
+		else:
+			enemy_that_we_care_about = get_minimal_enemy()
+	else:
+		enemy_that_we_care_about = get_minimal_enemy()
+	
+
+func get_minimal_enemy(check_closest_override:bool=false) -> Entity:
+	var minimal_value = INF
+	var minimal_unit:Entity = null
+	for enemy_faction_name in get_enemy_unit_factions():
+		# Skip our own faction, just in case it somehow ended up in enemy factions
+		if enemy_faction_name == cached_parent.faction_name:
+			continue
+
+		# Fetch all units belonging to this enemy faction	
+		var unit_array:Array = get_tree().get_nodes_in_group(enemy_faction_name)		
+		for enemy_unit:Entity in unit_array:
+			var enemy_unit_pos:Vector2i = enemy_unit.cur_pos
+			var dist_to_enemy:float = enemy_unit_pos.distance_to(cur_pos)
+			var used_value:float = dist_to_enemy
+			if (not check_closest_override and attack_behaviour == Hostile_Unit.who_to_attack.LOWEST_HEALTH):
+				used_value = enemy_unit.health
+			if used_value < minimal_value:
+				minimal_value = used_value
+				minimal_unit = enemy_unit
+	return minimal_unit
+
+func determine_friend_we_care_about() -> void:
+	friend_that_we_care_about = get_minimal_enemy()
+	
+func get_minimal_friendly(plan_pos:Vector2i, check_closest_override:bool=false) -> Entity:
+	var minimal_value = INF
+	var minimal_unit:Entity = null
+	for friendly_faction_name in get_friendly_factions():
+		var unit_array:Array = get_tree().get_nodes_in_group(friendly_faction_name)		
+		for friendly_unit:Entity in unit_array:
+			if friendly_unit == self:
+				continue
+			
+			var friendly_unit_pos:Vector2i = friendly_unit.cur_pos
+			var dist_to_friendly:float = friendly_unit_pos.distance_to(plan_pos)
+			var used_value:float = dist_to_friendly
+			if (not check_closest_override and support_behaviour == Hostile_Unit.who_to_support.LOWEST_HEALTH):
+				used_value = friendly_unit.health
+			if used_value < minimal_value:
+				minimal_value = used_value
+				minimal_unit = friendly_unit
+	return minimal_unit
+	
+
+
+func idle_state() -> void:
+	while not action_failed and not movement_failed:
+		determine_friend_we_care_about()
+		determine_enemy_we_care_about()
+		moving_state()
+		determine_friend_we_care_about()
+		determine_enemy_we_care_about()
+		acting_state()
+	done_state()
+	
+func done_state() -> void:
+	# Make sure these are zero so we get flagged as having ended our turn
 	action_count = 0
+	move_count = 0
+	
+func acting_state() -> void:
+	if action_count <= 0:
+		action_failed = true
+		return
+		
+	if unit_type_behaviour == type_of_unit.ATTACKER:
+		attacking_state()
+	elif unit_type_behaviour == type_of_unit.SUPPORTER:
+		supporting_state()
+		
+	if action_failed:
+		if unit_type_behaviour == type_of_unit.ATTACKER:
+			supporting_state()
+		elif unit_type_behaviour == type_of_unit.SUPPORTER:
+			attacking_state()
+	
+	return
+	
+func attacking_state() -> void:
+	if cached_attack_action == null:
+		action_failed = true
+		return
+	
+	if enemy_that_we_care_about.cur_pos not in cached_attack_action.range_pattern.calculate_affected_tiles_from_center(cur_pos):
+		action_failed = true
+		return
+	
+	use_action(cached_attack_action, enemy_that_we_care_about)
+	action_failed = false
+	return
+	
+func supporting_state() -> void:
+	if cached_support_action == null:
+		action_failed = true
+		return
+		
+	if friend_that_we_care_about.cur_pos not in cached_support_action.range_pattern.calculate_affected_tiles_from_center(cur_pos):
+		action_failed = true
+		return
+	
+	use_action(cached_support_action, friend_that_we_care_about)
+	action_failed = false
+	return
+	
+func moving_state() -> void:
+	if move_count <= 0:
+		movement_failed = true
+		return
+	
+	if should_we_should_retreat():
+		running_state()
+	else:
+		rushing_state()
+		
+	return
+	
+func should_we_should_retreat() -> bool:
+	match retreat_behaviour:
+		Hostile_Unit.when_to_retreat.NEVER:
+			return false
+		Hostile_Unit.when_to_retreat.LOW_HEALTH:
+			if health <= Globals.LOW_HEALTH_THRESHOLD:
+				return true
+		Hostile_Unit.when_to_retreat.MID_HEALTH:
+			if health <= Globals.MID_HEALTH_THRESHOLD:
+				return true
+		Hostile_Unit.when_to_retreat.WHEN_THREATENED:
+			for enemy_faction_name in get_enemy_unit_factions():
+				# Skip our own faction, just in case it somehow ended up in enemy factions
+				if enemy_faction_name == cached_parent.faction_name:
+					continue
+
+				# Fetch all units belonging to this enemy faction	
+				var unit_array:Array = get_tree().get_nodes_in_group(enemy_faction_name)		
+				for enemy_unit in unit_array:
+					if enemy_unit.cur_pos.distance_to(cur_pos) <= Globals.THREATENING_DISTANCE:
+						return true
+	return false
+	
+func calculate_heading(unit_coordinate:Vector2i, provided_coordinate:Vector2i) -> Vector2i:
+	return provided_coordinate - unit_coordinate
+	
+func convert_to_unit_vector(provided_coordinate:Vector2i) -> Vector2i:
+	var vector_length = provided_coordinate.length()
+	return provided_coordinate / vector_length
+	
+func retreat_to_furthest_point_from_closest_enemy() -> Vector2i:
+	var closest_enemy:Entity = null
+	if enemy_that_we_care_about != null and attack_behaviour == Hostile_Unit.who_to_attack.CLOSEST:
+		closest_enemy = enemy_that_we_care_about
+	else:
+		closest_enemy = get_minimal_enemy(true)
+	
+	var closest_enemy_coordinate:Vector2i = closest_enemy.cur_pos
+	if closest_enemy_coordinate != Vector2i(-INF, -INF):
+		var direction_to_enemy:Vector2i = calculate_heading(cur_pos, closest_enemy_coordinate)
+		var unit_vector:Vector2i = convert_to_unit_vector(direction_to_enemy)
+		return (-unit_vector * (move_count + 1)) + cur_pos
+	return Vector2i(-INF, -INF)
+	
+func retreat_to_friend() -> Vector2i:
+	var closest_unit:Entity = null
+	var closest_distance:float = INF
+	for faction_name_ref in get_friendly_factions():
+		# Iterate through all units in the friendly faction
+		for friendly_unit:Unit in get_tree().get_nodes_in_group(faction_name_ref):
+			# Exclude ourself
+			if friendly_unit == self:
+				continue
+			# Calculate the distance this friendly unit is from the queried coordinate
+			var friendly_unit_pos:Vector2i = friendly_unit.cur_pos
+			var dist_to_friendly:float = friendly_unit_pos.distance_to(cur_pos)
+			if dist_to_friendly < closest_distance:
+				closest_unit = friendly_unit
+				closest_distance = dist_to_friendly
+	if closest_unit == null:
+		return retreat_to_furthest_point_from_closest_enemy()
+	else:
+		var empty_tiles_around_friendly_unit:Array[Vector2i] = Globals.get_bfs_empty_tiles(cur_pos, 2, cached_parent.map_manager) 
+		var closest_tile:Vector2i = Vector2i(-INF, -INF)
+		closest_distance = INF
+		for adjacent_tile in empty_tiles_around_friendly_unit:
+			if adjacent_tile not in cached_parent.map_manager.map_dict_v2:
+				continue
+				
+			var dist_to_tile:float = cur_pos.distance_to(adjacent_tile)
+			if dist_to_tile < closest_distance:
+				closest_tile = adjacent_tile
+				closest_distance = dist_to_tile
+		if closest_tile != Vector2i(-INF, -INF):
+			return closest_tile
+		else:
+			return retreat_to_furthest_point_from_closest_enemy()
+	
+func running_state() -> void:
+	var retreat_coordinate:Vector2i = Vector2i(-INF, -INF)
+	if retreat_location_behaviour == Hostile_Unit.where_to_retreat_to.TO_FURTHEST_POINT_FROM_CLOSEST_ENEMY:
+		retreat_coordinate = retreat_to_furthest_point_from_closest_enemy()
+	else:
+		retreat_coordinate = retreat_to_friend()
+	
+	if retreat_coordinate == Vector2i(-INF, -INF):
+		movement_failed = true
+		return
+	
+	var pathfinder:Pathfinder = cached_parent.get_pathfinder(get_move_actions()[0])
+	var path_to_take:PackedVector2Array = pathfinder._return_path(cur_pos, retreat_coordinate)
+	if path_to_take[0] == Vector2(-INF, -INF):
+		movement_failed = true
+	else:
+		cached_parent.move_unit_via_path(self, path_to_take)
+	return
+	
+	
+func get_tiles_that_can_act_on_given_tile(target_unit:Entity, provided_action:Action) -> Array[Vector2i]:
+	var used_pattern:Pattern2D = provided_action.range_pattern
+	var possible_coordinates:Array[Vector2i] = []
+
+	# First, simulate being at a location wherein provided_coordinate falls within the pattern's grid 
+	
+	for coordinate_y in range(-used_pattern.grid_size.y, used_pattern.grid_size.y + 1):
+		for coordinate_x in range(-used_pattern.grid_size.x, used_pattern.grid_size.x + 1):
+			# Converts the pattern offsets in the pattern to an actual map coordinate
+			var offset_mapped_to_coordinate = target_unit.cur_pos + Vector2i(coordinate_x, coordinate_y)
+
+			# Secondly, see if the provided_coordinate is within the list of tiles affected from that location
+			var coordinates_affected_by_pattern = used_pattern.calculate_affected_tiles_from_center(offset_mapped_to_coordinate)
+			if target_unit.cur_pos in coordinates_affected_by_pattern:
+				# Thirdly, compile all pattern offsets wherein we can hit provided_coordinate with the pattern
+				possible_coordinates.append(offset_mapped_to_coordinate)
+	return possible_coordinates
+
+
+var cached_attack_action:Attackaction = null
+var cached_support_action:Healaction = null
+func get_point_to_act_from(is_attacking_target:bool, target_unit:Entity) -> Array:
+	var closest_point:Vector2i = Vector2i(-INF, -INF)
+	var closest_action:Action = null
+	var closest_distance:float = INF
+	var potential_actions:Array = get_attack_actions()
+	if not is_attacking_target:
+		potential_actions = get_restorative_actions()
+	
+	for action in potential_actions:
+		var points_that_can_hit_target:Array[Vector2i] = get_tiles_that_can_act_on_given_tile(target_unit, action)
+		for point in points_that_can_hit_target:
+			if point not in cached_parent.map_manager.map_dict_v2:
+				continue
+			
+			
+			var distance_to_point:float = point.distance_to(cur_pos)
+			if distance_to_point < closest_distance :
+				closest_point = point
+				closest_action = action
+				closest_distance = distance_to_point
+	return [closest_point, closest_action]
+	
+func rushing_state() -> void:
+	var acting_point = Vector2i(-INF, -INF)
+	cached_attack_action = null
+	cached_support_action = null
+	if unit_type_behaviour == type_of_unit.ATTACKER:
+		if enemy_that_we_care_about != null:
+			var returned_data:Array = get_point_to_act_from(true, enemy_that_we_care_about)
+			acting_point = returned_data[0]
+			cached_attack_action = returned_data[1]
+		elif friend_that_we_care_about != null:
+			var returned_data:Array = get_point_to_act_from(false, friend_that_we_care_about)
+			acting_point = returned_data[0]
+			cached_support_action = returned_data[1]
+	elif unit_type_behaviour == type_of_unit.SUPPORTER:
+		if friend_that_we_care_about != null:
+			var returned_data:Array = get_point_to_act_from(false, friend_that_we_care_about)
+			acting_point = returned_data[0]
+			cached_support_action = returned_data[1]
+		elif enemy_that_we_care_about != null:
+			var returned_data:Array = get_point_to_act_from(true, enemy_that_we_care_about)
+			acting_point = returned_data[0]
+			cached_attack_action = returned_data[1]
+	
+	if acting_point == Vector2i(-INF, -INF) or (cached_attack_action == null and cached_support_action == null):
+		movement_failed = true
+	else:
+		var pathfinder:Pathfinder = cached_parent.get_pathfinder()
+		var path_to_take:PackedVector2Array = pathfinder._return_path(cur_pos, acting_point)
+		if path_to_take[0] == Vector2(-INF, -INF):
+			movement_failed = true
+		else:
+			cached_parent.move_unit_via_path(self, path_to_take)
+	return

scenes/components/phase2/helpers/pathfinder.gd
@@ -38,23 +38,17 @@ func _rebuild_connections():
 			if vector in identifier_map:
 				connect_points(coord_id, identifier_map.get(vector))
 	
-func parse_point(provided_point:int) -> Vector2i:
-	if not has_point(provided_point):
-		return Vector2i(-1234, -1234)
-	else:
-		return Vector2i(get_point_position(provided_point))
-	
 func _return_path(provided_coordinate:Vector2i, provided_target:Vector2i):
-	var point_id:int = -1
-	var target_id:int = -1
+	var point_id:int = -INF
+	var target_id:int = -INF
 	if provided_coordinate in identifier_map:
 		point_id = identifier_map.get(provided_coordinate)
-	else:
-		point_id = get_closest_point(Vector2(provided_coordinate.x, provided_coordinate.y))
 	if provided_target in identifier_map:
 		target_id = identifier_map.get(provided_target)
-	else:
-		target_id = get_closest_point(Vector2(provided_target.x, provided_target.y))
+		
+	if point_id == -INF or target_id == -INF or point_id == null or target_id == null:
+		return [Vector2(-INF, -INF)]
+	
 	return get_point_path(point_id, target_id)
 	
 func calculate_path_cost(provided_path:PackedVector2Array):
@@ -64,6 +58,6 @@ func calculate_path_cost(provided_path:PackedVector2Array):
 		if cround in identifier_map:
 			total_cost += get_point_weight_scale(identifier_map.get(cround))
 		else:
-			total_cost += get_point_weight_scale(get_closest_point(cround))
+			return INF
 	return total_cost
 		

scenes/components/phase2/unit/NPC Unit.tscn
@@ -1,54 +1,9 @@
-[gd_scene load_steps=11 format=3 uid="uid://yhexcc5dvlyi"]
+[gd_scene load_steps=2 format=3 uid="uid://yhexcc5dvlyi"]
 
 [ext_resource type="Script" uid="uid://i4qgjsqm1kmh" path="res://scenes/components/phase2/unit/hostile_unit.gd" id="1_tp0p1"]
-[ext_resource type="Script" uid="uid://cfjj603id4kav" path="res://scenes/components/phase2/unit/states/state_machine.gd" id="2_innfo"]
-[ext_resource type="Script" uid="uid://c6bspt8577w13" path="res://scenes/components/phase2/unit/states/idle_state.gd" id="3_qpovp"]
-[ext_resource type="Script" uid="uid://bi0aofsx80qm2" path="res://scenes/components/phase2/unit/states/done_state.gd" id="4_hdn4k"]
-[ext_resource type="Script" uid="uid://bnjvimqbp8qkj" path="res://scenes/components/phase2/unit/states/acting_state.gd" id="5_kucia"]
-[ext_resource type="Script" uid="uid://cvwtke3k0khyb" path="res://scenes/components/phase2/unit/states/attacking_state.gd" id="6_d00tm"]
-[ext_resource type="Script" uid="uid://dnu43kl1snqds" path="res://scenes/components/phase2/unit/states/supporting_state.gd" id="7_h2ns1"]
-[ext_resource type="Script" uid="uid://cmoyk06yxfcj8" path="res://scenes/components/phase2/unit/states/moving_state.gd" id="8_7ut75"]
-[ext_resource type="Script" uid="uid://cbc8iicvg46p0" path="res://scenes/components/phase2/unit/states/running_state.gd" id="9_qcp63"]
-[ext_resource type="Script" uid="uid://p4ieyy2j4amo" path="res://scenes/components/phase2/unit/states/rushing_state.gd" id="10_0irhd"]
 
 [node name="NpcUnit" type="Node2D"]
 script = ExtResource("1_tp0p1")
 metadata/_custom_type_script = "uid://i4qgjsqm1kmh"
 
 [node name="AnimatedSprite2D" type="AnimatedSprite2D" parent="."]
-
-[node name="State_Machine" type="Node" parent="."]
-script = ExtResource("2_innfo")
-metadata/_custom_type_script = "uid://cfjj603id4kav"
-
-[node name="Idle_State" type="Node" parent="State_Machine"]
-script = ExtResource("3_qpovp")
-metadata/_custom_type_script = "uid://c6bspt8577w13"
-
-[node name="Done_State" type="Node" parent="State_Machine"]
-script = ExtResource("4_hdn4k")
-metadata/_custom_type_script = "uid://bi0aofsx80qm2"
-
-[node name="Acting_State" type="Node" parent="State_Machine"]
-script = ExtResource("5_kucia")
-metadata/_custom_type_script = "uid://bnjvimqbp8qkj"
-
-[node name="Attacking_State" type="Node" parent="State_Machine"]
-script = ExtResource("6_d00tm")
-metadata/_custom_type_script = "uid://cvwtke3k0khyb"
-
-[node name="Supporting_State" type="Node" parent="State_Machine"]
-script = ExtResource("7_h2ns1")
-metadata/_custom_type_script = "uid://dnu43kl1snqds"
-
-[node name="Moving_State" type="Node" parent="State_Machine"]
-script = ExtResource("8_7ut75")
-metadata/_custom_type_script = "uid://cmoyk06yxfcj8"
-
-[node name="Running_State" type="Node" parent="State_Machine"]
-script = ExtResource("9_qcp63")
-metadata/_custom_type_script = "uid://cbc8iicvg46p0"
-
-[node name="Rushing_State" type="Node" parent="State_Machine"]
-script = ExtResource("10_0irhd")
-metadata/_custom_type_script = "uid://p4ieyy2j4amo"

scenes/components/phase2/unit/states/acting_state.gd
@@ -1,46 +1,0 @@
-extends NPC_State
-class_name Acting_State
-
-func _ready() -> void:
-	state_name = "Acting_State"
-
-func _execute_state(prior_state:int, plan_pos:Vector2i, target_unit:Entity=null, target_action:Action=null) -> Array:
-	if cached_unit.attack_behaviour == Hostile_Unit.who_to_attack.LAST_TO_DAMAGE:
-		target_unit = choose_last_enemy_to_damage_me()
-	else:
-		target_unit = enemy_iterator(cached_unit.attack_behaviour)
-	
-	if target_unit != null:
-		return [State_Machine.states.RUSHING_STATE, plan_pos, target_unit]
-	else:
-		return [State_Machine.states.DONE_STATE, plan_pos, null]
-
-func select_attack_action() -> Attackaction:
-	var unit_atk_actions = cached_unit.get_attack_actions()
-	return unit_atk_actions[0]
-
-
-func choose_last_enemy_to_damage_me() -> Entity:
-	if cached_unit.last_unit_to_damage_me != null:
-		return cached_unit.last_unit_to_damage_me
-	else:
-		return enemy_iterator(Hostile_Unit.who_to_attack.CLOSEST)
-	
-func enemy_iterator(attack_behaviour:int) -> Entity:
-	var minimal_unit:Entity = null
-	var minimal_value:float = INF
-	for enemy_faction_name in cached_unit.get_enemy_unit_factions():
-		# Skip our own faction, just in case it somehow ended up in enemy factions
-		if enemy_faction_name == cached_unit.cached_parent.faction_name:
-			continue
-
-		# Fetch all units belonging to this enemy faction	
-		var unit_array:Array = get_tree().get_nodes_in_group(enemy_faction_name)		
-		for enemy_unit in unit_array:
-			var enemy_unit_value:float = enemy_unit.health
-			if attack_behaviour == Hostile_Unit.who_to_attack.CLOSEST:
-				enemy_unit_value = enemy_unit.cur_pos.distamce_to(cached_unit.cur_pos)
-			if enemy_unit_value < minimal_value:
-				minimal_unit = enemy_unit
-				minimal_value = enemy_unit_value
-	return minimal_unit

scenes/components/phase2/unit/states/acting_state.gd.uid
@@ -1,1 +1,0 @@
-uid://bnjvimqbp8qkj

scenes/components/phase2/unit/states/attacking_state.gd
@@ -1,10 +1,0 @@
-extends NPC_State
-class_name Attacking_State
-
-func _ready() -> void:
-	state_name = "Attacking_State"
-
-func _execute_state(prior_state:int, plan_pos:Vector2i, target_unit:Entity=null, target_action:Action=null) -> Array:
-	cached_parent.get_plan().add_item_to_plan(target_action, [], target_unit)
-	return [State_Machine.states.DONE_STATE, plan_pos,target_unit, target_action]	
-	

scenes/components/phase2/unit/states/attacking_state.gd.uid
@@ -1,1 +1,0 @@
-uid://cvwtke3k0khyb

scenes/components/phase2/unit/states/done_state.gd
@@ -1,9 +1,0 @@
-extends NPC_State
-class_name Done_State
-
-func _ready() -> void:
-	state_name = "Done_State"
-
-func _execute_state(prior_state:int, plan_pos:Vector2i, target_unit:Entity=null, target_action:Action=null) -> Array:
-	# This state doesn't actually do anything
-	return [State_Machine.states.DONE_STATE, plan_pos, target_unit, target_action]

commit c8b272c1d0b2c88b066b2555ee870d9a3698a919

fix: pathfinder argument bug
- In enemy.gd:
- - Fixes an accidental 'bug' introduced by commit 14e33bbeadbd87ed88c232056150bc0916b77901 from providing a move action to get_pathfinder() (as right now get_pathfinder doesn't search a dictionary of pathfinder objects for the one corresponding to that move pattern and return the matching one, but rather, just returns a pathfinder set for adjacent tiles)

scenes/components/phase2/unit/hostile_unit.gd
@@ -286,7 +286,7 @@ func running_state() -> void:
 		movement_failed = true
 		return
 	
-	var pathfinder:Pathfinder = cached_parent.get_pathfinder(get_move_actions()[0])
+	var pathfinder:Pathfinder = cached_parent.get_pathfinder()
 	var path_to_take:PackedVector2Array = pathfinder._return_path(cur_pos, retreat_coordinate)
 	if path_to_take[0] == Vector2(-INF, -INF):
 		movement_failed = true