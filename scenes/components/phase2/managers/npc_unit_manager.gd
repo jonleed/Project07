3e949db638e20c02d8acecd2b2cbd848f04c120d
45DegreeAngl <noguesaaron@gmail.com>
2025-11-24 20:17:57 -0500

    added enemy intent logic to the npc_unit_manager.gd

--- scenes/components/phase2/managers/npc_unit_manager.gd
+++ scenes/components/phase2/managers/npc_unit_manager.gd
@@ -23,6 +23,38 @@ func _step_turn():
 	if unused_units.is_empty():
 		end_turn()
 
+func end_turn() -> void:
+	modulate_important_enemy()
+	print(faction_name, " Turn End")
+	is_active = false
+	emit_signal("faction_turn_complete")
+
+var most_recent_modulated_enemy:Node2D = null
+
+func modulate_important_enemy()->void:
+	var chosen_enemy:Node2D = get_most_important_target()
+	if chosen_enemy:
+		if most_recent_modulated_enemy:
+			most_recent_modulated_enemy.modulate = Color.WHITE
+		most_recent_modulated_enemy = chosen_enemy
+		chosen_enemy.modulate = Color.RED
+
+func get_most_important_target()->Entity:
+	var ent_dict:Dictionary[Entity,int] = {}
+	var answer:Entity = null
+	
+	for unit in units:
+		if not unit is Hostile_Unit:
+			continue
+		unit.determine_enemy_we_care_about()
+		var cared_unit:Entity = unit.enemy_that_we_care_about
+		ent_dict[cared_unit] = ent_dict.get_or_add(cared_unit,0)+1
+		if ent_dict.get(cared_unit,0) > ent_dict.get(answer,0):
+			answer = cared_unit
+	print(ent_dict)
+	return answer
+
+
 # If we wait for onready, we seem to get issues with it being null?
 @onready var npc_unit_packed:PackedScene = preload("res://scenes/components/phase2/unit/NPC Unit.tscn")
 
