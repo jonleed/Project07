f01cee3e7176b006a4834c63a4f27d2ae8ae4e17
Bungus <154566159+45DegreeAngl@users.noreply.github.com>
2025-11-27 23:01:05 -0500

    more elegant fix to unit display

--- scenes/ui/phase2/components/ui scripts/unit_gui.gd
+++ scenes/ui/phase2/components/ui scripts/unit_gui.gd
@@ -10,8 +10,15 @@ func _ready():
 # Given array of Player Units, Updates UnitGUI
 func _on_update_unit_display(units):
 	await get_tree().process_frame
+	
+	##clean data of invalid units
+	var valid_units:Array = []
+	for unit in units:
+		if is_instance_valid(unit):
+			valid_units.append(unit)
+	
 	# Show boxes based on number of units
-	var count = units.size()
+	var count = valid_units.size()
 	if count<=0:
 		return
 	
@@ -20,7 +27,7 @@ func _on_update_unit_display(units):
 	
 	for i in count:
 		var box = $HBoxContainer.get_child(i)
-		var unit = units[i]
+		var unit = valid_units[i]
 		
 		if is_instance_valid(unit):
 			_populate_unit_box(box,unit,i)
@@ -30,47 +37,7 @@ func _on_update_unit_display(units):
 				box.modulate = Color.DIM_GRAY
 			box.visible = true
 	
-	if not $HBoxContainer.get_child(0).visible:
-		##if the first one is invisible, we need to show that one, so adjust all
-		for child in $HBoxContainer.get_children():
-			child.visible = false
-		
-		if count == 1:
-			var box = $HBoxContainer.get_child(0)
-			var unit = units[0]
-			_populate_unit_box(box,unit,0)
-			return
-		
-		for i in count-1:
-			var box = $HBoxContainer.get_child(i)
-			var unit = units[i+1]
-			
-			if is_instance_valid(unit):
-				_populate_unit_box(box,unit,i)
-				if unit.move_count>0 or unit.action_count>0:
-					box.modulate = Color.WHITE
-				else:
-					box.modulate = Color.DIM_GRAY
-				box.visible = true
 	
-		#else:
-			#_populate_unit_box(box,unit,clamp(i-1,0,count))
-	
-	#for i in range(4):
-		#var box = $HBoxContainer.get_node("UnitBox%d" % (i + 1))
-		##set visibility by validity
-		##box.visible = i < count
-		#
-		#if i < count:
-			#var unit = units[i]
-			###do get or add logic to hide already displayed units
-			#if displayed_units.get(unit,false) or unit == null:
-				#box.visible = false
-			#else:
-				#displayed_units.set(unit,unit)
-				#box.visible = true
-			#
-			#_populate_unit_box(box, unit, i)
 
 
 # Populates Portaits and HP
